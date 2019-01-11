b6a53579633a2b05777db6aca8efec7be2e3bfa1
jeffzhang <zhangjunfeng01@weidian.com>
2019-01-11 22:38:20 +0800

    2019.01.11 project init

--- src/layouts/components/layout/MainSidebar/MainSidebar.vue
+++ src/layouts/components/layout/MainSidebar/MainSidebar.vue
@@ -5,7 +5,7 @@
           <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
             <div class="d-table m-auto">
               <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="@/assets/images/shards-dashboards-logo.svg" alt="Shards Dashboard">
-              <span v-if="!hideLogoText" class="d-none d-md-inline ml-1">Shards Dashboard</span>
+              <span v-if="!hideLogoText" class="d-none d-md-inline ml-1">Megrez Scanner</span>
             </div>
           </a>
           <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none" @click="handleToggleSidebar()">
--- src/router.js
+++ src/router.js
@@ -9,7 +9,8 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
+  base: '/',
   linkActiveClass: 'active',
   linkExactActiveClass: 'exact-active',
   scrollBehavior() {
