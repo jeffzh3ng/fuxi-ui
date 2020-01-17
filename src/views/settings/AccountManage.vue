76a9385dafc9eacdf82d514d466feb792333f20b
jeffzhang <jeffzh3ng@gmail.com>
2020-01-17 23:47:11 +0800

    date: 2020_0117_2347

--- src/views/settings/AccountManage.vue
+++ src/views/settings/AccountManage.vue
@@ -1,10 +1,76 @@
 <template>
     <div>
-        <d-button size="sm" class="btn-accent ml-auto ml-1">
+        <d-button @click="openModalNewUser = true" size="sm" class="btn-accent ml-auto ml-1 mr-4">
             <i class="material-icons mr-1">library_add</i>
             <strong>New User</strong>
         </d-button>
-        <table class="table mt-2">
+        <d-button size="sm" class="btn-accent ml-auto ml-1 ml-4">
+            <i class="material-icons mr-1">lock_open</i>
+            <strong>Change Password</strong>
+        </d-button>
+        <Modal
+                v-model="openModalNewUser"
+                scrollable
+                footer-hide
+                title="New Account">
+            <div>
+                <d-row>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span class="text-danger mr-1">*</span><span>Username</span>
+                        <Input v-model="userData.username" class="pt-1" placeholder="Username..."/>
+                    </d-col>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span>Nick</span>
+                        <Input v-model="userData.nick" class="pt-1" placeholder="Not required"/>
+                    </d-col>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span>E-Mail</span>
+                        <Input v-model="userData.email" class="pt-1" placeholder="Not required"/>
+                    </d-col>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span class="text-danger mr-1">*</span><span>Password</span>
+                        <Input v-model="userData.password" class="pt-1" placeholder="Password" type="password"/>
+                        <Input v-model="userData.password_v" class="pt-1" placeholder="Verify" type="password"/>
+                    </d-col>
+                    <d-col class="pt-4 text-right" lg="12" md="12" sm="12">
+                        <Button @click="newUser">
+                            <i class="material-icons mr-1">add_box</i>
+                            Submit
+                        </Button>
+                    </d-col>
+                </d-row>
+            </div>
+        </Modal>
+        <Modal
+                v-model="openModalEditUser"
+                scrollable
+                footer-hide
+                title="Modify User Information">
+            <div>
+                <d-row>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span class="text-danger mr-1">*</span><span>Username</span>
+                        <Input v-model="tempUserData.username" class="pt-1" placeholder="Username..."/>
+                    </d-col>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span>Nick</span>
+                        <Input v-model="tempUserData.nick" class="pt-1" placeholder="Not required"/>
+                    </d-col>
+                    <d-col lg="12" md="12" sm="12" class="pt-3">
+                        <span>E-Mail</span>
+                        <Input v-model="tempUserData.email" class="pt-1" placeholder="Not required"/>
+                    </d-col>
+
+                    <d-col class="pt-4 text-right" lg="12" md="12" sm="12">
+                        <Button @click="editUser(tempUserData.uid)">
+                            <i class="material-icons mr-1">add_box</i>
+                            Submit
+                        </Button>
+                    </d-col>
+                </d-row>
+            </div>
+        </Modal>
+        <table class="table mt-4">
             <thead class="bg-light">
                 <tr>
                     <th scope="col" class="border-0 text-center">#</th>
@@ -25,8 +91,11 @@
                     <td>{{item['email']}}</td>
                     <td>{{item['date']}}</td>
                     <td class="text-center">
+                        <Tooltip placement="top" content="Edit" theme="light">
+                            <Icon @click="openEditUserModal(item['uid'], item['username'], item['nick'], item['email'])" class="mx-2" title="Edit" size="21" type="ios-create" />
+                        </Tooltip>
                         <Tooltip placement="top" content="Delete" theme="light">
-                            <Icon class="mx-3" title="Delete" size="21" type="ios-trash" />
+                            <Icon @click="deleteUser(item['uid'])" title="Delete" size="21" type="ios-trash" />
                         </Tooltip>
                     </td>
                 </tr>
@@ -55,6 +124,21 @@
                 pageSize: 10,
                 pageCurrent: 1,
                 reqApiPath: "/settings/user",
+                openModalNewUser: false,
+                userData: {
+                    username: "",
+                    nick: "",
+                    email: "",
+                    password: "",
+                    password_v: "",
+                },
+                tempUserData: {
+                    uid: "",
+                    username: "",
+                    nick: "",
+                    email: "",
+                },
+                openModalEditUser: false,
             }
         },
         mounted() {
@@ -91,6 +175,88 @@
                         this.spinShow = false
                     }
                 })
+            },
+            newUser() {
+                if (this.userData.username.length === 0 ){
+                    this.$message.error("Please enter username!");
+                    return
+                }
+                if (this.userData.password_v !== this.userData.password) {
+                    this.$message.error("Please check you password!");
+                } else {
+                    let data = {
+                        username: this.userData.username,
+                        nick: this.userData.nick,
+                        email: this.userData.email,
+                        password: this.userData.password,
+                    };
+                    this.openModalNewUser = false;
+                    this.$axios.post("/admin", data).then(response => {
+                        let status = response['data']['status'];
+                        if (status['status'] === 'success') {
+                            this.$message.success(status['message']);
+                            this.openModalNewUser = false;
+                            this.userData = {
+                                username: "",
+                                nick: "",
+                                email: "",
+                                password: "",
+                                password_v: "",
+                            };
+                            this.getData()
+                        } else {
+                            this.$message.error(status['message']);
+                            this.getData()
+                        }
+                    })
+                }
+            },
+            openEditUserModal(uid, username, nick, email) {
+                this.tempUserData.uid = uid;
+                this.tempUserData.username = username;
+                this.tempUserData.nick = nick;
+                this.tempUserData.email = email;
+                this.openModalEditUser = true;
+            },
+            editUser(uid) {
+                this.openModalEditUser = false;
+                let data = this.tempUserData;
+                this.tempUserData = {
+                    uid: "",
+                    username: "",
+                    nick: "",
+                    email: "",
+                };
+                this.$axios.put(this.reqApiPath + "/" + uid, data).then(response => {
+                    let status = response['data']['status'];
+                    if (status['status'] === 'success') {
+                        this.$message.success(status['message']);
+                        this.getData()
+                    } else {
+                        this.$message.error(status['message']);
+                        this.getData()
+                    }
+                })
+            },
+            deleteUser(uid) {
+                this.$Modal.confirm({
+                    title: 'WARNING',
+                    content: 'Are you sure you want to delete this account?',
+                    closable: true,
+                    okText: 'OK',
+                    cancelText: 'Cancel',
+                    onOk: () => {
+                        this.$axios.delete(this.reqApiPath + "/" + uid).then(response => {
+                            let status = response['data']['status'];
+                            if (status['status'] === 'success') {
+                                this.$message.success(status['message']);
+                                this.getData()
+                            } else {
+                                this.$message.error(status['message']);
+                                this.getData()
+                            }
+                        })
+                    }});
             },
         }
     }
